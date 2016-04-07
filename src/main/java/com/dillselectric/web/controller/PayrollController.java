a06f889f93146342a60cd5f6342a17e8eb12ab4f
safari137 <dbdill137@gmail.com>
2016-04-07 09:24:08 -0400

    cleaned up dependencies

--- src/main/java/com/dillselectric/web/controller/PayrollController.java
+++ src/main/java/com/dillselectric/web/controller/PayrollController.java
@@ -5,15 +5,11 @@ import com.dillselectric.web.view_model.EmployeePayrun;
 import com.dillselectric.web.view_model.PayRunContainer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
